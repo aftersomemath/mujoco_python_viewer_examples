2009e9881b2fc2dad8a7c8bbba0789df08131513
Levi Burner <leviburner@gmail.com>
2024-05-29 13:05:39 -0400

    Programmatically estimate double pendulum linearization, closes #1

--- double_pendulum/double_pendulum.py
+++ double_pendulum/double_pendulum.py
@@ -6,25 +6,24 @@ import numpy as np
 from scipy.linalg import solve_continuous_are
 
 # Find a K matrix for a linearized double pendulum using LQR
-def double_pendulum_lqr_K():
-  # System linearization calculated using "derivative" sample
-  A = np.array([[0, 0, 1, 0],
-                [0, 0, 0, 1],
-                [ 2.494531E+01, -1.565526E+01, -2.130544E-01,  5.164733E-01],
-                [-3.252928E+01,  6.589209E+01,  5.164733E-01, -1.793546E+00],
-               ])
-  B = np.array([[0], [0], [2.130544E+00], [-5.164733E+00]])
+def double_pendulum_lqr_K(m, d):
+    # Calculate system linearization
+    A = np.zeros((2 * m.nv + m.na, 2 * m.nv + m.na))
+    B = np.zeros((2 * m.nv + m.na, m.nu))
+    mujoco.mjd_transitionFD(m, d, 1e-7, 1, A=A, B=B, C=None, D=None)
 
-  Q = 1 * np.array([[10,  0,  0,   0],
-                    [0, 100,  0,   0],
-                    [0,   0, 10,   0],
-                    [0,   0,  0, 100]])
-  R = 1 * np.eye(1)
+    # Convert to continuous time
+    A = A - np.eye(A.shape[0])
+    A = A / m.opt.timestep
+    B = B / m.opt.timestep
 
-  S = solve_continuous_are(A, B, Q, R)
+    Q = 1 * np.diag((10, 10, 100, 100))
+    R = np.eye(B.shape[1])
 
-  K = np.linalg.inv(R)@(B.T @ S)
-  return K
+    S = solve_continuous_are(A, B, Q, R)
+
+    K = np.linalg.inv(R) @ (B.T @ S)
+    return K
 
 def double_pendulum_control(m, d, K):
   x = np.concatenate((
@@ -45,11 +44,13 @@ def load_callback(m=None, d=None):
   d = mujoco.MjData(m)
 
   if m is not None:
+    # Calculate K matrix
+    K = double_pendulum_lqr_K(m, d)
+
     # Set some initial conditions
     d.joint('shoulder').qpos = 0.1
     d.joint('elbow').qpos = 0.1
     # Set the control callback
-    K = double_pendulum_lqr_K()
     mujoco.set_mjcb_control(lambda m, d: double_pendulum_control(m, d, K))
 
   return m, d
